64bb29898424e324836a3eb51ea04574b12691bc
AravindanSridhar <aravindansridhars@gmail.com>
2019-02-06 18:57:23 +0530

    First Deployment Version

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require(path.join(
+  __dirname,
+  "insta-studio-vellore-firebase-adminsdk-ul7ko-4f9e15d4da.json"
+));
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://insta-studio-vellore.firebaseio.com"
+});
+
+//Database Reference
+var db = admin.database();
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
@@ -32,8 +47,17 @@ app.get("/productspic", function(req, res) {
   res.sendFile(path.join(__dirname, "/res/products.jpg"));
 });
 
+app.get("/log", function(req, res) {
+  res.sendFile(path.join(__dirname, "/res/logothumbnail.ico"));
+});
+
 app.get("/gokulpic", function(req, res) {
   res.sendFile(path.join(__dirname, "/res/gokul.jpg"));
+});
+
+app.get("/toasts", function(req, res) {
+  console.log("dd");
+  res.sendFile(path.join(__dirname, "/res/bulma-toast.min.js"));
 });
 
 app.get("/pradeeppic", function(req, res) {
@@ -46,6 +70,22 @@ app.get("/googleplay", function(req, res) {
 
 app.get("/bg", function(req, res) {
   res.sendFile(path.join(__dirname, "/res/bg.jpg"));
+});
+
+app.post("/request", function(req, res) {
+  var check = db.ref("requests/" + req.body.phone);
+  check.once("value", function(data) {
+    if (data.val() == null) {
+      var ref = db.ref("requests");
+      var a = ref.child(req.body.phone);
+      a.set(req.body);
+      res.send("Hi " + req.body.name + " your request is being processed");
+    } else {
+      res.send(
+        "Hi " + data.val().name + " you have already requested a quote."
+      );
+    }
+  });
 });
 
 app.listen(3000, function() {
